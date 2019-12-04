211115a8cbd40ed753d7a8f8c70fc9c30c3ec8d5
chad kidd <ckiddwebdev@gmail.com>
2019-12-04 14:32:59 -0800

    functions done after some syntax issues it's working

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,22 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+        # note to self true and false NEED to be capitalized otherwise invalid
 
-# Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
-
+# Read a number from the keyboard
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+if (num % 2) == 0:
+  print(f'{num} is EVEN!')
+else:
+  print(f'{num} is Odd!')
 
+# works in pipenv woohoo!!
